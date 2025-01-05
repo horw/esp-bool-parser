88dff99b6908736555ca08680a570c6153255f73
igor udot <horw7001@gmail.com>
2025-01-05 14:52:30 +0300

    hello world

--- lala_esp_bool_parser/__init__.py
+++ lala_esp_bool_parser/__init__.py
@@ -7,7 +7,7 @@ Tools for building ESP-IDF related apps.
 
 # ruff: noqa: E402
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .bool_parser import parse_bool_expr, register_addition_attribute
 
